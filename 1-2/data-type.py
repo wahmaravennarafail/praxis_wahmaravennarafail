0d1f135daade49b3041a229c47294f88e7a3cf87
wahmaravennarafail <wahmaravenna78@gmail.com>
2021-10-07 11:09:16 +0700

    pertemuan2

---
+++ 1-2/data-type.py
@@ -0,0 +1,11 @@
+#type data pada python
+
+#string
+a = 'Hallo'
+
+#dictionary
+datadiri = {'Nama' : 'Wahma Ravenna Rafail', 'Alamat' : 'Rembang', 'Gender' : 'Perempuan', 'Hobi' : 'Nonton Drakor'}
+print(datadiri)
+print(type(datadiri))
+#tampilkan satu index
+print(datadiri['Nama'])
