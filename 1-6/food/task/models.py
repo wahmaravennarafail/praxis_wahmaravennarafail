51119f0f4c1be546deccc9682b87b0ae6877f050
wahmaravennarafail <wahmaravenna78@gmail.com>
2021-10-12 17:04:06 +0700

    pertemuan 7|membuat program kasir restaurant

--- 1-6/food/task/models.py
+++ 1-6/food/task/models.py
@@ -1,10 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField
 from django.db.models.fields import IntegerField
 
 # Create your models here.
 from django.db import models
-form django.db.models.base import Model
-from django.db.models.fields import CharField
 
 class makanan(models.Model):
     jenis = models.CharField(max_length=200)
@@ -16,8 +15,8 @@ class minuman(models.Model):
     nama = CharField(max_length=200)
     harga = models.IntegerField()
 
-class minuman(models.Model):
+class pesanan(models.Model):
     jenis = models.ForeignKey(makanan, on_delete=models.CASCADE)
     Jumlah_makanan = models.IntegerField()
-    minuman = models.ForeignKey(minuman, on_delete=models.CASADANE)
+    minuman = models.ForeignKey(minuman, on_delete=models.CASCADE)
     jumlah_minuman = models.IntegerField()
--- 1-6/food/task/urls.py
+++ 1-6/food/task/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index), 
-    path('makanan/', views.makanan), 
+    path('makanan/', views.makanan),
+    path('minuman/', views.minuman),
+    path('pesanan/', views.pesanan),
     path ('hapus/<id>/', views.hapus),
     path('detail/<id>/', views.detail),
     path('edit/<id>/', views.edit),
